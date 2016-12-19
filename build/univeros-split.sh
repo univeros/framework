dfa94188ecf7c06f1858c73ec5003936cb351263
Antonio Ramirez <amigo.cobos@gmail.com>
2016-12-19 01:52:01 +0100

    add Container subsplit

--- build/univeros-split.sh
+++ build/univeros-split.sh
@@ -1,3 +1,4 @@
 git subsplit init git@github.com:univeros/framework.git
 git subsplit publish --heads="master" --no-tags src/Altair/Structure:git@github.com:univeros/structure.git
+git subsplit publish --heads="master" --no-tags src/Altair/Container:git@github.com:univeros/container.git
 rm -rf .subsplit/
