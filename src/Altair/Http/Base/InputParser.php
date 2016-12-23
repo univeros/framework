b386df0fc1a9751dc5fe8fc7fe54cb8bbbd22329
Antonio Ramirez <amigo.cobos@gmail.com>
2016-12-23 13:30:18 +0100

    minor fix

--- src/Altair/Http/Base/InputParser.php
+++ src/Altair/Http/Base/InputParser.php
@@ -58,6 +58,7 @@ class InputParser implements InputInterface
 
         return is_object($body) && $body instanceof JsonSerializable
             ? $body->jsonSerialize()
-            : $body;
+            // if parsed body is an object but doesn't implements JsonSerializable use json parsing instead
+            : is_object($body) ? json_decode(json_encode($body), true) : $body;
     }
 }
