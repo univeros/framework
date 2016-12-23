090e92bc975f35473e11a0ce07ba35dd6af52fc9
Antonio Ramirez <amigo.cobos@gmail.com>
2016-12-23 15:15:09 +0100

    add Payload

--- src/Altair/Http/Base/Payload.php
+++ src/Altair/Http/Base/Payload.php
@@ -1,7 +1,168 @@
 <?php
 namespace Altair\Http\Base;
 
-class Payload
+use Altair\Http\Collection\InputCollection;
+use Altair\Http\Collection\SettingsCollection;
+use Altair\Http\Contracts\PayloadInterface;
+use Altair\Structure\Map;
+
+class Payload implements PayloadInterface
 {
-    // TODO
+    /**
+     * @var string
+     */
+    protected $status;
+    /**
+     * @var InputCollection
+     */
+    protected $inputCollection;
+    /**
+     * @var array
+     */
+    protected $output = [];
+    /**
+     * @var array
+     */
+    protected $messages = [];
+    /**
+     * @var Map|null
+     */
+    protected $settingsCollection;
+
+    /**
+     * Payload constructor.
+     *
+     * @param InputCollection|null $inputCollection
+     * @param SettingsCollection|null $settingsCollection
+     */
+    public function __construct(InputCollection $inputCollection = null, SettingsCollection $settingsCollection = null)
+    {
+        $this->inputCollection = $inputCollection?? new InputCollection();
+        $this->settingsCollection = $settingsCollection?? new SettingsCollection();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function withStatus(string $status): PayloadInterface
+    {
+        $cloned = clone $this;
+        $cloned->status = $status;
+
+        return $cloned;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function withInputCollection(InputCollection $inputCollection): PayloadInterface
+    {
+        $cloned = clone $this;
+        $cloned->inputCollection = $inputCollection;
+
+        return $cloned;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getInputCollection(): InputCollection
+    {
+        return $this->inputCollection;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function withOutput(array $output): PayloadInterface
+    {
+        $cloned = clone $this;
+        $cloned->output = $output;
+
+        return $cloned;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getOutput(): array
+    {
+        return $this->output;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function withMessages(array $messages): PayloadInterface
+    {
+        $cloned = clone $this;
+        $cloned->messages = $messages;
+
+        return $cloned;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getMessages(): array
+    {
+        return $this->messages;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function withSetting(string $name, $value): PayloadInterface
+    {
+        $cloned = clone $this;
+        $cloned->settingsCollection->put($name, $value);
+
+        return $cloned;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function withoutSetting(string $name): PayloadInterface
+    {
+        $cloned = clone $this;
+        $cloned->settingsCollection->remove($name);
+
+        return $cloned;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function withSettingsCollection(SettingsCollection $settingsCollection): PayloadInterface
+    {
+        $cloned = clone $this;
+        $cloned->settingsCollection = $settingsCollection;
+
+        return $cloned;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSetting(string $name, $default = null)
+    {
+        return $this->settingsCollection->get($name);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSettingsCollection(): SettingsCollection
+    {
+        return $this->settingsCollection;
+    }
 }
--- src/Altair/Http/Contracts/PayloadInterface.php
+++ src/Altair/Http/Contracts/PayloadInterface.php
@@ -1,6 +1,9 @@
 <?php
 namespace Altair\Http\Contracts;
 
+use Altair\Http\Collection\InputCollection;
+use Altair\Http\Collection\SettingsCollection;
+
 interface PayloadInterface extends HttpStatusInterface
 {
     /**
@@ -12,7 +15,7 @@ interface PayloadInterface extends HttpStatusInterface
      *
      * @return PayloadInterface
      */
-    public function withStatus($status): PayloadInterface;
+    public function withStatus(string $status): PayloadInterface;
 
     /**
      * Get the status of the payload.
@@ -24,20 +27,20 @@ interface PayloadInterface extends HttpStatusInterface
     public function getStatus(): int;
 
     /**
-     * Create a copy of the payload with input array.
+     * Create a copy of the payload with input collection map.
      *
-     * @param array $input
+     * @param InputCollection $inputCollection
      *
      * @return PayloadInterface
      */
-    public function withInput(array $input): PayloadInterface;
+    public function withInputCollection(InputCollection $inputCollection): PayloadInterface;
 
     /**
      * Get input array from the payload.
      *
-     * @return array
+     * @return InputCollection
      */
-    public function getInput(): array;
+    public function getInputCollection(): InputCollection;
 
     /**
      * Create a copy of the payload with output array.
@@ -79,7 +82,7 @@ interface PayloadInterface extends HttpStatusInterface
      *
      * @return PayloadInterface
      */
-    public function withSetting($name, $value): PayloadInterface;
+    public function withSetting(string $name, $value): PayloadInterface;
 
     /**
      * Create a copy of the payload without a setting.
@@ -88,21 +91,31 @@ interface PayloadInterface extends HttpStatusInterface
      *
      * @return PayloadInterface
      */
-    public function withoutSetting($name): PayloadInterface;
+    public function withoutSetting(string $name): PayloadInterface;
 
     /**
-     * Get a payload setting.
+     * Create a copy of the payload with settings collection map.
+     *
+     * @param SettingsCollection $settingsCollection
+     *
+     * @return PayloadInterface
+     */
+    public function withSettingsCollection(SettingsCollection $settingsCollection): PayloadInterface;
+
+    /**
+     * Get a payload setting. Default if not found.
      *
      * @param string $name
+     * @param mixed $default
      *
      * @return mixed
      */
-    public function getSetting($name);
+    public function getSetting(string $name, $default = null);
 
     /**
      * Get all payload settings.
      *
-     * @return array
+     * @return SettingsCollection
      */
-    public function getSettings(): array;
+    public function getSettingsCollection(): SettingsCollection;
 }
