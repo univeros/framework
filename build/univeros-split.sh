62a562e259cd5b59e82249461ef51b7ddbc19fed
Antonio Ramirez <amigo.cobos@gmail.com>
2017-01-04 00:59:47 +0100

    add missing packages for the git subsplit build script

--- build/univeros-split.sh
+++ build/univeros-split.sh
@@ -1,4 +1,10 @@
 git subsplit init git@github.com:univeros/framework.git
-git subsplit publish --heads="master" --no-tags src/Altair/Structure:git@github.com:univeros/structure.git
+git subsplit publish --heads="master" --no-tags src/Altair/Structure:git@github.com:univeros/base.git
+git subsplit publish --heads="master" --no-tags src/Altair/Structure:git@github.com:univeros/configuration.git
 git subsplit publish --heads="master" --no-tags src/Altair/Container:git@github.com:univeros/container.git
+git subsplit publish --heads="master" --no-tags src/Altair/Container:git@github.com:univeros/cookie.git
+git subsplit publish --heads="master" --no-tags src/Altair/Container:git@github.com:univeros/http.git
+git subsplit publish --heads="master" --no-tags src/Altair/Container:git@github.com:univeros/queue.git
+git subsplit publish --heads="master" --no-tags src/Altair/Container:git@github.com:univeros/security.git
+git subsplit publish --heads="master" --no-tags src/Altair/Structure:git@github.com:univeros/structure.git
 rm -rf .subsplit/
