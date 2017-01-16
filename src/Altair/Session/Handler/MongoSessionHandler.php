6fb697c6ca4fdd237ef7d66dc9355be6bad65b57
Antonio Ramirez <amigo.cobos@gmail.com>
2017-01-16 02:02:34 +0100

    classes placeholder to build some custom session handlers

---
+++ src/Altair/Session/Handler/MongoSessionHandler.php
@@ -0,0 +1,38 @@
+<?php
+namespace Altair\Session\Handler;
+
+use SessionHandlerInterface;
+
+class MongoSessionHandler implements SessionHandlerInterface
+{
+    public function open($savePath, $sessionName)
+    {
+        // TODO: Implement open() method.
+    }
+
+    public function close()
+    {
+        // TODO: Implement close() method.
+    }
+
+    public function read($sessionId)
+    {
+        // TODO: Implement read() method.
+    }
+
+    public function write($sessionId, $data)
+    {
+        // TODO: Implement write() method.
+    }
+
+    public function destroy($sessionId)
+    {
+        // TODO: Implement destroy() method.
+    }
+
+    public function gc($maxlifetime)
+    {
+        // TODO: Implement gc() method.
+    }
+
+}
