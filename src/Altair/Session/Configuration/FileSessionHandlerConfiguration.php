57760ca136502f50c4b2ded33a10fe7accf5d27b
Antonio Ramirez <amigo.cobos@gmail.com>
2017-02-26 19:46:03 +0100

    re #1 add Filesessionhandler  class

--- src/Altair/Session/Configuration/FileSessionHandlerConfiguration.php
+++ src/Altair/Session/Configuration/FileSessionHandlerConfiguration.php
@@ -2,12 +2,25 @@
 namespace Altair\Session\Configuration;
 
 use Altair\Configuration\Contracts\ConfigurationInterface;
+use Altair\Configuration\Traits\EnvAwareTrait;
 use Altair\Container\Container;
+use Altair\Container\Definition;
+use Altair\Session\Handler\FileSessionHandler;
+use SessionHandlerInterface;
 
 class FileSessionHandlerConfiguration implements ConfigurationInterface
 {
+    use EnvAwareTrait;
+
     public function apply(Container $container)
     {
-        // TODO: Implement apply() method.
+        $definition = new Definition([
+            ':path' => $this->env->get('SESSION_FILE_PATH'),
+            ':minutes' => $this->env->get('SESSION_FILE_MINUTES')
+        ]);
+
+        $container
+            ->define(FileSessionHandler::class, $definition)
+            ->alias(SessionHandlerInterface::class, FileSessionHandler::class);
     }
 }
