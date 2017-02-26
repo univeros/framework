baadbd3f1d5f4ab1c2d667e2adba2f03cbcf816e
Antonio Ramirez <amigo.cobos@gmail.com>
2017-02-26 19:53:58 +0100

    re #1 add SessionManager configuration

--- src/Altair/Session/Configuration/SessionManagerConfiguration.php
+++ src/Altair/Session/Configuration/SessionManagerConfiguration.php
@@ -3,11 +3,16 @@ namespace Altair\Session\Configuration;
 
 use Altair\Configuration\Contracts\ConfigurationInterface;
 use Altair\Container\Container;
+use Altair\Session\Contracts\SessionManagerInterface;
+use Altair\Session\SessionManager;
 
 class SessionManagerConfiguration implements ConfigurationInterface
 {
     public function apply(Container $container)
     {
-        // TODO: Implement apply() method.
+        // This manager should be working with Altair's Http Component
+        // ServerRequestInterface is already configured on HttpMessageConfiguration::class
+        // ensure the application makes use of it.
+        $container->alias(SessionManagerInterface::class, SessionManager::class);
     }
 }
